080144f8be2f3a0bc864f4d902ff3dabc3024ee5
lessw2020 <lessw@etrillium.com>
2023-04-07 16:13:32 +0000

    model arg parsing

--- training.py
+++ training.py
@@ -34,3 +34,39 @@ from torch.utils.data import DistributedSampler
 
 # prep -------
 colorama.init(autoreset=True)  # reset after every line
+
+# --- globals
+_valid_models = ["dev", "mini"]
+
+
+def training_main():
+    print(f"Welcome! ")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="PyTorch experiments with NanoGPT and DistCompiler"
+    )
+    parser.add_argument(
+        "--model",
+        default="mini",
+        metavar="string",
+        choices=_valid_models,
+        help="choose NanoGPT model to run, available: `mini`, `dev`, (default: mini)",
+    )
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    curr_model = args.model
+    print(f"===> Loading Model: {args.model=}")
+    assert (
+        curr_model in _valid_models
+    ), f"Model {curr_model} is not supported. Check valid list in {__name__}. Aborting..."
+
+    if curr_model in ["dev", "mini"]:
+        import config.shakespeare_config as config
+
+    training_main()
